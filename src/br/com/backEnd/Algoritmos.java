d38883cecc8f80243df1233c462803b2ba3ce47e
Andredlima23 <andredlima23@gmail.com>
2020-05-22 00:34:33 -0300

    ajuste de telas

--- src/br/com/backEnd/Algoritmos.java
+++ src/br/com/backEnd/Algoritmos.java
@@ -2,6 +2,5 @@ package br.com.backEnd;
 
 public class Algoritmos {
 //Fazer um random de 0 a 8 e retornar uma sendo que a 2ª é diferente da 1ª, e a 3ª diferente da 2ª e da 1ª.
-	double sortear = Math.random() * 8;
 	
 }
--- src/br/com/frontEnd/TelaIntro.java
+++ src/br/com/frontEnd/TelaIntro.java
@@ -1,10 +1,19 @@
 package br.com.frontEnd;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 public class TelaIntro extends JFrame {
@@ -34,9 +43,43 @@ public class TelaIntro extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 500);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(204, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JLabel cabeçalho = new JLabel("SoccerQuiz");
+		cabeçalho.setFont(new Font("Showcard Gothic", Font.PLAIN, 30));
+		
+		JLabel lblNewLabel = new JLabel(new ImageIcon(getClass().getResource("bola.png")));
+		
+		JButton btnNewButton = new JButton("Come\u00E7ar");
+		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(121)
+					.addComponent(cabeçalho, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(119))
+				.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 424, Short.MAX_VALUE)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(161)
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 103, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(160, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(cabeçalho, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 356, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
+					.addGap(16))
+		);
+		contentPane.setLayout(gl_contentPane);
+		
+		
 	}
-
 }
