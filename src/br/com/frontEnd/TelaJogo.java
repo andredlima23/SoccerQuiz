e3266a58ab3e8d7ea0813d4ab6ac72f4d79ff8c6
Andredlima23 <andredlima23@gmail.com>
2020-05-26 20:52:05 -0300

    alteração dos radiobuttons

--- src/br/com/frontEnd/TelaJogo.java
+++ src/br/com/frontEnd/TelaJogo.java
@@ -58,52 +58,52 @@ public class TelaJogo extends JFrame {
 		cabecalho.setHorizontalAlignment(SwingConstants.CENTER);
 		cabecalho.setFont(new Font("Showcard Gothic", Font.PLAIN, 30));
 
-		JRadioButton alternativa1 = new JRadioButton();
-		alternativa1.setBounds(10, 343, 21, 21);
-		alternativa1.setFont(new Font("Times New Roman", Font.BOLD, 20));
-		alternativa1.setBackground(new Color(204, 255, 255));
-
 		JRadioButton alternativa2 = new JRadioButton();
-		alternativa2.setBounds(10, 235, 21, 21);
-		alternativa2.setBackground(new Color(204, 255, 255));
+		alternativa2.setBounds(10, 343, 21, 21);
 		alternativa2.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		alternativa2.setBackground(new Color(204, 255, 255));
+
+		JRadioButton alternativa4 = new JRadioButton();
+		alternativa4.setBounds(10, 235, 21, 21);
+		alternativa4.setBackground(new Color(204, 255, 255));
+		alternativa4.setFont(new Font("Times New Roman", Font.BOLD, 20));
 
 		JRadioButton alternativa3 = new JRadioButton();
 		alternativa3.setBounds(10, 271, 21, 21);
 		alternativa3.setBackground(new Color(204, 255, 255));
 		alternativa3.setFont(new Font("Times New Roman", Font.BOLD, 20));
 
-		JRadioButton alternativa4 = new JRadioButton();
-		alternativa4.setBounds(10, 307, 21, 21);
-		alternativa4.setBackground(new Color(204, 255, 255));
-		alternativa4.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		JRadioButton alternativa1 = new JRadioButton();
+		alternativa1.setBounds(10, 307, 21, 21);
+		alternativa1.setBackground(new Color(204, 255, 255));
+		alternativa1.setFont(new Font("Times New Roman", Font.BOLD, 20));
 
 		ButtonGroup buttonGroup = new ButtonGroup();
-		buttonGroup.add(alternativa1);
 		buttonGroup.add(alternativa2);
-		buttonGroup.add(alternativa3);
 		buttonGroup.add(alternativa4);
+		buttonGroup.add(alternativa3);
+		buttonGroup.add(alternativa1);
 
+		JTextArea opcao4 = new JTextArea();
+		opcao4.setEditable(false);
+		opcao4.setFont(new Font("Times New Roman", Font.BOLD, 16));
+		opcao4.setBounds(33, 235, 375, 22);
+		opcao4.setBackground(new Color(204, 255, 255));
 		JTextArea opcao1 = new JTextArea();
 		opcao1.setEditable(false);
 		opcao1.setFont(new Font("Times New Roman", Font.BOLD, 16));
-		opcao1.setBounds(33, 235, 375, 22);
+		opcao1.setBounds(33, 271, 375, 22);
 		opcao1.setBackground(new Color(204, 255, 255));
 		JTextArea opcao2 = new JTextArea();
 		opcao2.setEditable(false);
 		opcao2.setFont(new Font("Times New Roman", Font.BOLD, 16));
-		opcao2.setBounds(33, 271, 375, 22);
+		opcao2.setBounds(33, 307, 375, 22);
 		opcao2.setBackground(new Color(204, 255, 255));
 		JTextArea opcao3 = new JTextArea();
 		opcao3.setEditable(false);
 		opcao3.setFont(new Font("Times New Roman", Font.BOLD, 16));
-		opcao3.setBounds(33, 307, 375, 22);
+		opcao3.setBounds(33, 343, 375, 22);
 		opcao3.setBackground(new Color(204, 255, 255));
-		JTextArea opcao4 = new JTextArea();
-		opcao4.setEditable(false);
-		opcao4.setFont(new Font("Times New Roman", Font.BOLD, 16));
-		opcao4.setBounds(33, 343, 375, 22);
-		opcao4.setBackground(new Color(204, 255, 255));
 		
 		JButton btnNewButton = new JButton("Responder");
 		btnNewButton.setBounds(153, 395, 150, 30);
@@ -120,10 +120,10 @@ public class TelaJogo extends JFrame {
 				
 				int posicaoRespostaCorreta = Algoritmos.getPosicaoRespostaCorreta(question.getRespostas(), respostaCorreta);
 				
-				opcao1.setText(question.getRespostas().get(0));
-				opcao2.setText(question.getRespostas().get(1));
-				opcao3.setText(question.getRespostas().get(2));
-				opcao4.setText(question.getRespostas().get(3));
+				opcao4.setText(question.getRespostas().get(0));
+				opcao1.setText(question.getRespostas().get(1));
+				opcao2.setText(question.getRespostas().get(2));
+				opcao3.setText(question.getRespostas().get(3));
 					
 				
 			}
@@ -132,14 +132,14 @@ public class TelaJogo extends JFrame {
 		getContentPane().setLayout(null);
 		getContentPane().add(cabecalho);
 		getContentPane().add(textArea);
-		getContentPane().add(alternativa4);
 		getContentPane().add(alternativa1);
-		getContentPane().add(alternativa3);
 		getContentPane().add(alternativa2);
+		getContentPane().add(alternativa3);
+		getContentPane().add(alternativa4);
+		getContentPane().add(opcao1);
 		getContentPane().add(opcao2);
 		getContentPane().add(opcao3);
 		getContentPane().add(opcao4);
-		getContentPane().add(opcao1);
 		getContentPane().add(btnNewButton);
 	}
 }
